bf9050063f01eb50be3aa373d78f4334a396074d
linder <achie_31@yahoo.com>
2024-09-21 19:10:58 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution here!
+let cats = ["Milo","Otis","Garfield"];
+ function destructivelyAppendCat(){
+    return cats.push("Ralph");
+ }
+ function destructivelyPrependCat(){
+    return cats.unshift("Bob");
+ }
+ function destructivelyRemoveLastCat(){
+    return cats.pop();
+ }
+ function destructivelyRemoveFirstCat(){
+    return cats.shift();
+ }
+ function appendCat(name) {
+    return [...cats, name]; 
+  }
+function prependCat(name) {
+    return [name, ...cats]; 
+  }
+function removeLastCat() {
+    return cats.slice(0, cats.length - 1); 
+  }
+  function removeFirstCat() {
+    return cats.slice(1); 
+  }
